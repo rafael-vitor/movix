70af81b58d1d97191574020c6f5f07d1b7f8742d
ravi <rafael.vitor.r2@gmail.com>
2021-03-03 18:39:55 -0300

    Add fallback to movie poster

--- src/components/MovieList/index.tsx
+++ src/components/MovieList/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./styles.css";
 
-import { AiOutlineFire } from "react-icons/ai";
 import { DiscoverDataType } from "../../types";
 
 type Props = {
@@ -23,14 +22,22 @@ function MovieList({ listTitle, data, icon }: Props) {
               <p>{m.original_title}</p>
               <p>{m.release_date.split("-")[0]}</p>
             </div>
+            {m.poster_path ? (
               <img
                 className="MovieList-item-image"
                 src={`https://image.tmdb.org/t/p/w200/${m.poster_path}`}
-              ></img>
-            </div>
-          ))}
-        </div>
+                alt="movie poster"
+              />
+            ) : (
+              <div className="MovieList-poster-fallback">
+                <p>{m.original_title}</p>
+                <p>{m.release_date.split("-")[0]}</p>
+              </div>
+            )}
+          </div>
+        ))}
       </div>
+    </div>
   );
 }
 
