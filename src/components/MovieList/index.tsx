1b55df28ffdd55ed2b606817aeaa54d8b494c80c
ravi <rafael.vitor.r2@gmail.com>
2021-03-03 22:23:37 -0300

    Filter by rating on movie list

--- src/components/MovieList/index.tsx
+++ src/components/MovieList/index.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import "./styles.css";
+import React, { useState } from "react";
+import { AiOutlineStar, AiFillStar } from "react-icons/ai";
 
 import { DiscoverDataType } from "../../types";
+import "./styles.css";
 
 type Props = {
   listTitle: string;
@@ -10,17 +11,54 @@ type Props = {
 };
 
 function MovieList({ listTitle, data, icon }: Props) {
+  const [selectedRating, setSelectedRating] = useState<number>(0);
+  const [filterActive, setFilterActive] = useState<boolean>(false);
+
+  const onChangeRating = (rating: number) => {
+    if (filterActive && selectedRating === rating) {
+      setFilterActive(false);
+      setSelectedRating(0);
+    } else {
+      setFilterActive(true);
+      setSelectedRating(rating);
+    }
+  };
+
+  const movies = filterActive
+    ? data?.results.filter(
+        (m) =>
+          m.vote_average / 2 >= selectedRating - 1 &&
+          m.vote_average / 2 < selectedRating
+      )
+    : data?.results;
+
   return (
     <div className="MovieList">
-      <div className="MovieList-title">
+      <div className="MovieList-header">
         {icon} {listTitle}
+        <div className="MovieList-rating">
+          {[...Array(5).keys()].map((_, idx) => {
+            const Icon =
+              selectedRating <= idx || !filterActive
+                ? AiOutlineStar
+                : AiFillStar;
+            return (
+              <Icon
+                onClick={() => onChangeRating(idx + 1)}
+                key={idx}
+                color={filterActive ? "red" : "lightgrey"}
+                size="28px"
+              />
+            );
+          })}
+        </div>
       </div>
       <div className="MovieList-list">
-        {data?.results.map((m) => (
+        {movies.map((m) => (
           <div className="MovieList-item" key={m.id}>
             <div className="MovieList-item-overlay">
               <p>{m.original_title}</p>
-              <p>{m.release_date.split("-")[0]}</p>
+              <p>{m.release_date?.split("-")[0]}</p>
             </div>
             {m.poster_path ? (
               <img
